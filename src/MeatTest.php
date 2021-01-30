5bde0a31041bd7d93f859e9f0a32efc0e2ee0ffe
ignace nyamagana butera <nyamsprod@gmail.com>
2021-01-30 16:35:35 +0100

    fix meat tests

--- src/MeatTest.php
+++ src/MeatTest.php
@@ -37,29 +37,29 @@ final class MeatTest extends TestCase
     }
 
     /** @test */
-    public function it_can_create_jambon_meat(): void
+    public function it_can_create_ham_meat(): void
     {
         $sauce = Meat::ham();
 
-        self::assertSame('jambon', $sauce->name());
+        self::assertSame('ham', $sauce->name());
         self::assertEquals(Euro::fromCents(200), $sauce->price());
     }
 
     /** @test */
-    public function it_can_create_jambon_meat_with_specified_unit_price(): void
+    public function it_can_create_ham_meat_with_specified_unit_price(): void
     {
         $price = Euro::fromCents(400);
         $sauce = Meat::ham($price);
 
-        self::assertSame('jambon', $sauce->name());
+        self::assertSame('ham', $sauce->name());
         self::assertSame($price, $sauce->price());
     }
 
     /** @test */
-    public function it_fails_creating_a_jambon_meat_with_invalid_price(): void
+    public function it_fails_creating_a_ham_meat_with_invalid_price(): void
     {
         $this->expectException(UnableToHandleIngredient::class);
-        $this->expectExceptionMessage('`jambon` can not be priced at `-0.01 EUR`.');
+        $this->expectExceptionMessage('`ham` can not be priced at `-0.01 EUR`.');
 
         Meat::ham(price: Euro::fromCents(-1));
     }
