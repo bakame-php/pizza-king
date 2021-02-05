f5efbc322900e6b2693248978640a55520ae1e03
ignace nyamagana butera <nyamsprod@gmail.com>
2021-02-05 22:35:38 +0100

    Using Slim to exposes REST API endpoints

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Bakame\PizzaKing\Controller\ComposePizzaFromIngredients;
+use Bakame\PizzaKing\Controller\ComposePizzaFromName;
+use Bakame\PizzaKing\Model\Pizzaiolo;
+use Bakame\PizzaKing\Service\ExceptionToProblemConverter;
+use Crell\ApiProblem\HttpConverter;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\StreamFactory;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$pizzaiolo = new Pizzaiolo();
+$responseFactory = new ResponseFactory();
+$streamFactory = new StreamFactory();
+$converter = new ExceptionToProblemConverter();
+$httpConverter = new HttpConverter($responseFactory);
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(displayErrorDetails: true, logErrors: true, logErrorDetails: true);
+$errorMiddleware->setDefaultErrorHandler(fn (
+    ServerRequestInterface $request,
+    Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails,
+    ?LoggerInterface $logger = null,
+): ResponseInterface => $httpConverter->toJsonResponse($converter->toApiProblem($exception)));
+
+$app->post('/compose', new ComposePizzaFromIngredients($pizzaiolo, $responseFactory, $streamFactory));
+$app->get('/pizza/{name}', new ComposePizzaFromName($pizzaiolo, $responseFactory, $streamFactory));
+$app->run();
